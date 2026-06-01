b1b4f7fc36ec45e747fe2af1a6959ab293f53867
James Woolfenden <james.woolfenden@gmail.com>
2026-06-01 09:04:52 +0100

    Fix Terraform replication block syntax in secrets.tf

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -3,20 +3,28 @@
 
 resource "google_secret_manager_secret" "anthropic_api_key" {
   secret_id = "anthropic-api-key"
-  replication { auto {} }
+  replication {
+    auto {}
+  }
 }
 
 resource "google_secret_manager_secret" "garmin_api_secret" {
   secret_id = "garmin-api-secret"
-  replication { auto {} }
+  replication {
+    auto {}
+  }
 }
 
 resource "google_secret_manager_secret" "vapid_private_key" {
   secret_id = "vapid-private-key"
-  replication { auto {} }
+  replication {
+    auto {}
+  }
 }
 
 resource "google_secret_manager_secret" "internal_secret" {
   secret_id = "fuel-internal-secret"
-  replication { auto {} }
+  replication {
+    auto {}
+  }
 }
