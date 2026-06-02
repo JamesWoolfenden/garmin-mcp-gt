93a5c8a09672b53d4f8d4f4922a79f7fa508cb40
James Woolfenden <james.woolfenden@gmail.com>
2026-06-02 13:50:24 +0100

    Import pre-created allowed-emails and admin-uid secrets into Terraform state

---
+++ terraform/imports.tf
@@ -0,0 +1,9 @@
+import {
+  id = "projects/pike-477416/secrets/fuel-allowed-emails"
+  to = google_secret_manager_secret.allowed_emails
+}
+
+import {
+  id = "projects/pike-477416/secrets/fuel-admin-uid"
+  to = google_secret_manager_secret.admin_uid
+}
